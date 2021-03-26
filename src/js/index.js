5f870ff5a56381f1db4bd5230f3a2e2e7e0b90b4
Anna Udziela <annaudziela@gmail.com>
2021-03-26 16:14:21 +0100

    height for front cards changed, array with elements to shuffle created

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,15 @@ console.log('Did you try? 🙇‍♂️')
 
 let selectedPair = [];
 
+let elementsToShuffle = document.querySelector('.grid__list');
+let classesToShuffle = Array.from(elementsToShuffle.children);
+
+console.log(classesToShuffle);
+
+const shuffleButton = document.querySelector('.shuffle');
+shuffleButton.addEventListener('click', () => {
+    console.log(classesToShuffle.sort(() => Math.random() - 0.5));
+})
 
 
 function clickedClass(e) {
@@ -27,8 +36,6 @@ function clickedClass(e) {
         } else {
             // alert('it is not a pair, try again');
             console.log(selectedPair);
-
-// opóźnić pętlę for i osobno wywołać czyszczenie tablicy
             
             for (const openedElement of selectedPair) {
                 setTimeout(() => {
@@ -47,50 +54,11 @@ for (const elementsClass of classCheck) {
     elementsClass.addEventListener('click', clickedClass);
 }
 
-for (const elementsClass of classCheck) {
-    elementsClass.addEventListener('click', () => {
-
-        let visibility = false;
-
-        if (visibility) {
-            document.documentElement.style.setProperty('--display-one', 'none');
-            document.documentElement.style.setProperty('--display-two', 'block');
-            visibility = false;
-        } else {
-            document.documentElement.style.setProperty('--display-one', 'block');
-            document.documentElement.style.setProperty('--display-two', 'none');
-            visibility = true;
-        }
-    })
-}
-
-
-
-// const testDiv = document.querySelector('.test-div');
-// testDiv.addEventListener('click', () => {
-//     testDiv.innerHTML = targetClass;
+// const shuffleButton = document.querySelector('.shuffle');
+// shuffleButton.addEventListener('click', () => {
+//     newArray.sort(() => Math.random() - 0.5);
 // })
 
-// const gridElement = document.querySelectorAll('.grid__element');
-
-// for (const elements of gridElement) {
-//     elements.addEventListener('click', () => {
-//         elements.innerHTML = 'tile changed';
-//     })
-// }
-
-const b = gridElement[6];
-console.log(`${b}`);
-
-console.log(`${gridElement.length}`);
-const newArray = Array.from(gridElement);
-
-
-const shuffleButton = document.querySelector('.shuffle');
-shuffleButton.addEventListener('click', () => {
-    newArray.sort(() => Math.random() - 0.5);
-})
-
-const shuffle = newArray.sort(() => Math.random() - 0.5);
-document.onload = shuffle;
-console.log(shuffle);
+// const shuffle = newArray.sort(() => Math.random() - 0.5);
+// document.onload = shuffle;
+// console.log(shuffle);
